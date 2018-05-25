cc0d15137993bca116775dda2c07aab07a58bd2d
madhuripatil506 <madhuripatil506@gmail.com>
2018-05-25 17:14:08 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/********************************************************************************
+ *  Purpose         : controller for index.html that can handle data
+ *  @file           : index.js
+ *  @author         : Madhuri chaudhari
+ *  @version        : 1.0
+ *  @since          : 25-05-2018
+ ********************************************************************************/
 var app = angular.module('app', ['ngMaterial','firebase']);
 app.controller('appCtrl', function($scope, $firebase) {
   var ref = new Firebase("https://studentdemo-bf174.firebaseio.com");
