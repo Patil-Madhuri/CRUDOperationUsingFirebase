64c971606acd1b338c888198dd2917c814a27a46
madhuripatil506 <madhuripatil506@gmail.com>
2018-05-25 17:43:43 +0530

    commit

--- index.js
+++ index.js
@@ -5,25 +5,24 @@
  *  @version        : 1.0
  *  @since          : 25-05-2018
  ********************************************************************************/
-var app = angular.module('app', ['ngMaterial','firebase']);
+var app = angular.module('app', ['ngMaterial', 'firebase']);
 app.controller('appCtrl', function($scope, $firebase) {
   var ref = new Firebase("https://studentdemo-bf174.firebaseio.com");
   var sync = $firebase(ref);
 
   $scope.DB = sync.$asArray();
-  $scope.title = 'student app';
+  $scope.title = 'Student App';
   $scope.app = {
     name: '',
     sirname: '',
     contact: ''
   }
   $scope.add = function() {
-    console.log("hii");
     $scope.DB.$add($scope.app);
     $scope.app = {
       name: '',
       sirname: '',
-      contact : ''
+      contact: ''
     }
   }
   $scope.edit = function(value) {
@@ -32,4 +31,5 @@ app.controller('appCtrl', function($scope, $firebase) {
   $scope.delete = function(item) {
     $scope.DB.$remove(item)
   }
+
 });
