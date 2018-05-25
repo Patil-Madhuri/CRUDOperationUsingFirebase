e33068258df4328531ab4564dc89af55966d5b1f
madhuripatil506 <madhuripatil506@gmail.com>
2018-05-25 18:00:22 +0530

    commit

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.controller('appCtrl', function($scope, $firebase) {
   var ref = new Firebase("https://studentdemo-bf174.firebaseio.com");
   var sync = $firebase(ref);
 
+/*
+  $asArray() returns the data from database in the form of array
+*/
   $scope.DB = sync.$asArray();
   $scope.title = 'Student App';
   $scope.app = {
